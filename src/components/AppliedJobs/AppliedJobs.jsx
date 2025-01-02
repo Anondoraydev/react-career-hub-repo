fa40677140784543280bac5940fc7ac4547a0e23
Anondo Ray <Anondo554@gmail.com>
2025-01-03 00:02:39 +0600

    add full data

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { AiFillDollarCircle } from "react-icons/ai";
+import { FaLocationDot } from "react-icons/fa6";
 import { useLoaderData } from "react-router-dom";
 import { getStordJobAppliction } from "../utility/localStorge";
 
@@ -42,10 +44,34 @@ const AppliedJobs = () => {
 			<ul>
 				{appliedJobs.map((job) => (
 					<li key={job.id}>
-						<span>
-							{job.job_title}
-							{job.company_name} : {job.remote_or_onsite}
-						</span>
+						<div className="flex justify-between items-center">
+							<div className="flex">
+								<img
+									src={job.logo}
+									alt=""
+									className="py-16 px-5 w-[200px] mt-5 bg-gray-200 border"
+								/>
+								<div className="pl-5 pt-5">
+									<h3 className="text-xl font-bold">{job.job_title}</h3>
+									<p className="py-2 text-xl">{job.company_name}</p>
+									<button className="px-6 py-2 font-extrabold border rounded text-[#9873FF] pb-4 pt-4 border-teal-200 mr-4 ">
+										{job.remote_or_onsite}
+									</button>
+									<button className="px-6 py-2 font-extrabold border rounded text-[#9873FF] pb-4 pt-4 border-teal-200 mr-4">
+										{job.job_type}
+									</button>
+									<div className="flex gap-8 py-5">
+										<p className="flex gap-2">
+											<FaLocationDot className="mt-1" /> {job.location}
+										</p>
+										<p className="flex gap-2">
+											<AiFillDollarCircle className="mt-1" /> {job.salary}
+										</p>
+									</div>
+								</div>
+							</div>
+							<button className="btn btn-primary">View Details</button>
+						</div>
 					</li>
 				))}
 			</ul>
